74a51339f6ddd7d8280f12c5f6808ef5f01255c0
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-02-27 17:42:58 +0700

    Init project

---
+++ src/main/java/com/khoahd7621/Application.java
@@ -0,0 +1,7 @@
+package com.khoahd7621;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello Oracle Certified Professional Java SE 17 Developer!");
+    }
+}
