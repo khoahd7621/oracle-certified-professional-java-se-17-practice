35d2ee6b5525e706ff89bd01071006ae5896c2d2
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-03-08 14:15:25 +0700

    Chapter 2 - 1. Understanding java operator

---
+++ src/main/java/com/khoahd7621/Chapter2_Operators/I_UnderstandingJavaOperator.java
@@ -0,0 +1,50 @@
+package com.khoahd7621.Chapter2_Operators;
+
+public class I_UnderstandingJavaOperator {
+    // Java operation
+    // var c = a + b;
+    // a, b, c are operands
+    // + is operator
+    // a + b is expression => result assigned to c
+    // a + b = c is statement
+    // = is assignment operator. It assigns the value of the right operand to the left operand.
+
+    // Types of Java Operators
+    // 1. Unary Operator (applied to one operand)
+    // 2. Binary Operator (applied to two operands)
+    // 3. Ternary Operator (applied to three operands)
+
+    // Operator Precedence
+    // 1. Unary Operator
+    // 2. Multiplicative Operator (*, /, %)
+    // 3. Additive Operator (+, -)
+    // 4. Relational Operator (<, >, <=, >=)
+    // 5. Equality Operator (==, !=)
+    // 6. Logical Operator (&&, ||)
+    // 7. Assignment Operator (=, +=, -=, *=, /=, %=)
+
+    // Operator                         Symbols and examples                    Evaluation
+    // Post-unary operators             expression++, expression--              Left-to-right
+    // Pre-unary operators              ++expression, --expression              Left-to-right
+    // Other unary operators            -, !, ~, +, (type)                      Right-to-left
+    // Cast                             (Type)reference                         Right-to-left
+    // Multiplication/division/modulus  *, /, %                                 Left-to-right
+    // Addition/subtraction             +, -                                    Left-to-right
+    // Shift operators                  <<, >>, >>>                             Left-to-right
+    // Relational operators             <, >, <=, >=, instanceof                Left-to-right
+    // Equal to/not equal to            ==, !=                                  Left-to-right
+    // Logical AND                      &                                       Left-to-right
+    // Logical exclusive OR             ^                                       Left-to-right
+    // Logical inclusive OR             |                                       Left-to-right
+    // Conditional AND                  &&                                      Left-to-right
+    // Conditional OR                   ||                                      Left-to-right
+    // Ternary operators                boolean expression ? expression1 :      Right-to-left
+    //                                  expression2
+    // Assignment operators             =, +=, -=, *=, /=, %=, &=, ^=,          Right-to-left
+    //                                  |=, <<=, >>=, >>>=
+    // Arrow operator                   ->                                      Right-to-left
+
+    // The arrow operator (->), sometimes called the arrow function or lambda
+    // operator, is a binary operator that represents a relationship between
+    // two operands.
+}
